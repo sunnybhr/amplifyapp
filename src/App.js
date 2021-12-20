2347ddcf85d6c21fbe985bcb729407a2cf285a01
SUNNY BHRARA <u7063427@anu.edu.au>
2021-12-20 13:50:35 +1100

    change for v2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React, { Component } from "react"
 import logo from './logo.svg';
 import './App.css';
-/* import { Component } from 'react/cjs/react.production.min'; 
+/* import { Component } from 'react/cjs/react.production.min'; */
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
 }
 
-*/
+
+
+/*
 
 class App extends Component {
   render() {
@@ -38,5 +32,7 @@ class App extends Component {
     )
   }
 }
+
+*/
 
 export default App;
